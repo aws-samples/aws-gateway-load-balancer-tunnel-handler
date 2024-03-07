5961e149375236fb40f7bca7e34bba1fb1c277f8
Marcus Lean <marcuslean@gmail.com>
2024-03-07 16:02:12 +1100

    fix: compile error

--- utils.h
+++ utils.h
@@ -13,6 +13,14 @@
 #include <cstdarg>  // For va_start, etc.
 #include <chrono>
 #include <vector>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#ifndef SYS_gettid
+#error "SYS_gettid unavailable on this system"
+#endif
+
+#define gettid() ((pid_t)syscall(SYS_gettid))
 
 using namespace std::string_literals;
 
