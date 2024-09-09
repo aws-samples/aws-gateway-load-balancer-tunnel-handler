c76dd5cbcda9e262215c534a0ca1bf6d26ff6faa
Andrew Gray <grayaw@amazon.com>
2024-09-09 11:40:18 -0600

    Switch to sysctl calls

--- example-scripts/create-route.sh
+++ example-scripts/create-route.sh
@@ -3,6 +3,6 @@
 echo "==> Setting up to simply route incoming packets back out"
 echo Mode is $1, In Int is $2, Out Int is $3, ENI is $4
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
-echo 0 > /proc/sys/net/ipv4/conf/$2/rp_filter
+sysctl net.ipv4.ip_forward=1
+sysctl net.ipv4.conf.$2.rp_filter=0
+sysctl net.ipv6.conf.all.forwarding=1
