2fb45c6942685e00470072081f8ead019302e77b
Andrew Gray <grayaw@amazon.com>
2025-01-29 10:06:33 -0700

    Update create-net.sh to detect outbound interface instead of eth0 hardcode

--- example-scripts/create-nat.sh
+++ example-scripts/create-nat.sh
@@ -5,13 +5,16 @@
 
 echo Mode is $1, In Int is $2, Out Int is $3, ENI is $4
 
+# Get our output interface (eth0, ens5, etc)
+OUTINT=`ip route show default | cut -f 5 -d ' '`
+
 if [ "$1" = "CREATE" ]; then
   echo "==> Setting up two-armed NAT"
 
-  iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-  iptables -A FORWARD -i $2 -o eth0 -j ACCEPT
-  ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-  ip6tables -A FORWARD -i $2 -o eth0 -j ACCEPT
+  iptables -t nat -A POSTROUTING -o $OUTINT -j MASQUERADE
+  iptables -A FORWARD -i $2 -o $OUTINT -j ACCEPT
+  ip6tables -t nat -A POSTROUTING -o $OUTINT -j MASQUERADE
+  ip6tables -A FORWARD -i $2 -o $OUTINT -j ACCEPT
 
   echo 1 > /proc/sys/net/ipv4/ip_forward
   echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
@@ -20,8 +23,8 @@ if [ "$1" = "CREATE" ]; then
 else
   echo "==> Destroying up two-armed NAT"
 
-  iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-  iptables -D FORWARD -i $2 -o eth0 -j ACCEPT
-  ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-  ip6tables -D FORWARD -i $2 -o eth0 -j ACCEPT
+  iptables -t nat -D POSTROUTING -o $OUTINT -j MASQUERADE
+  iptables -D FORWARD -i $2 -o $OUTINT -j ACCEPT
+  ip6tables -t nat -D POSTROUTING -o $OUTINT -j MASQUERADE
+  ip6tables -D FORWARD -i $2 -o $OUTINT -j ACCEPT
 fi
